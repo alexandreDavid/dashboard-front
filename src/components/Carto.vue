401f05851098b81878b1cbb1ec1dc19531f53a81
Alexandre David <adavid@rheagroup.lan>
2018-04-20 16:52:01 +0100

    Located on Uganda

--- src/components/Carto.vue
+++ src/components/Carto.vue
@@ -11,13 +11,13 @@ import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
 export default {
   name: 'Carto',
   components: { LMap, LTileLayer, LMarker },
-  data() {
+  data () {
     return {
-      zoom: 13,
-      center: L.latLng(47.413220, -1.219482),
-      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      zoom: 8,
+      center: L.latLng(1.384226, 32.640962),
+      url:'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
       attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(47.413220, -1.219482),
+      marker: L.latLng(1.384226, 32.640962)
     }
   }
 }
@@ -29,4 +29,3 @@ export default {
     width: 100%;
   }
 </style>
-
