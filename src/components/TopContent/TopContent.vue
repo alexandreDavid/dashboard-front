c178fcd0210ec1060a47c697756df1cffbd6abca
Alexandre David <adavid@rheagroup.lan>
2018-05-01 16:25:00 +0100

    Async call && bootstrap classes

--- src/components/TopContent/TopContent.vue
+++ src/components/TopContent/TopContent.vue
@@ -2,11 +2,11 @@
   <div id="top-content" class="over-map">
     <div class="over-map-left">
       <SearchLocationResult v-if="searchLocationResult" v-bind:searchLocationResult="searchLocationResult"></SearchLocationResult>
-      <div style="position: relative;">
-        <img src="../../assets/logo.png" style="width: 38px;display: inline-block;vertical-align: top;">
+      <div class="position-relative">
+        <img src="../../assets/logo.png" class="align-top d-inline-block" style="width: 38px;">
         <SearchLocation @input="onSearchLocationSelected" class="search-location"></SearchLocation>
-        <button type="button" class="btn btn-primary" style="vertical-align: top;"><font-awesome-icon :icon="iconLocate" /></button>
-        <button type="button" class="btn btn-primary d-inline-block d-sm-none" style="vertical-align: top;"><font-awesome-icon :icon="iconMenu" /></button>
+        <button type="button" class="btn btn-primary align-top"><font-awesome-icon :icon="iconLocate" /></button>
+        <button type="button" class="btn btn-primary d-inline-block d-sm-none align-top"><font-awesome-icon :icon="iconMenu" /></button>
       </div>
     </div>
     <div class="over-map-right">
@@ -18,7 +18,7 @@
 <script>
 import SearchLocation from './SearchLocation/SearchLocation'
 import Managing from './Managing/Managing'
-import SearchLocationResult from './SearchLocationResult/SearchLocationResult'
+
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import faLocationArrow from '@fortawesome/fontawesome-free-solid/faLocationArrow'
 import faBars from '@fortawesome/fontawesome-free-solid/faBars'
@@ -27,7 +27,7 @@ export default {
   name: 'TopContent',
   components: {
     SearchLocation,
-    SearchLocationResult,
+    SearchLocationResult: () => import('./SearchLocationResult/SearchLocationResult'),
     Managing,
     FontAwesomeIcon
   },
