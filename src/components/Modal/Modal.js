0966cf00a804a8b51ba300181b50b740881ebbb2
Alexandre David <adavid@rheagroup.lan>
2018-05-02 11:18:45 +0100

    Modal beautifying

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 
 export const Modal = Vue.component('Modal', {
-  template: '#modal-template'
+  template: '#modal-template',
+  computed: {
+    hasHeaderSlot () {
+      return !!this.$slots['header']
+    },
+    hasBodySlot () {
+      return !!this.$slots['body']
+    },
+    hasFooterSlot () {
+      return !!this.$slots['footer']
+    }
+  }
 })
--- src/components/Modal/ModalInit.vue
+++ src/components/Modal/ModalInit.vue
@@ -1,36 +1,4 @@
-<template>
-  <script type="text/x-template" id="modal-template">
-    <transition name="modal">
-      <div class="modal-mask">
-        <div class="modal-wrapper">
-          <div class="modal-container">
-
-            <div class="modal-header">
-              <slot name="header">
-                default header
-              </slot>
-            </div>
-
-            <div class="modal-body">
-              <slot name="body">
-                default body
-              </slot>
-            </div>
-
-            <div class="modal-footer">
-              <slot name="footer">
-                default footer
-                <button class="modal-default-button" @click="$emit('close')">
-                  OK
-                </button>
-              </slot>
-            </div>
-          </div>
-        </div>
-      </div>
-    </transition>
-  </script>
-</template>
+<template src="./modal.tpl.html"></template>
 
 <script>
 export default {
@@ -52,13 +20,13 @@ export default {
   transition: opacity .3s ease;
 }
 
-.modal-wrapper {
+/* .modal-wrapper {
   display: table-cell;
   vertical-align: middle;
 }
 
 .modal-container {
-  width: 300px;
+  min-width: 300px;
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
@@ -79,7 +47,7 @@ export default {
 
 .modal-default-button {
   float: right;
-}
+} */
 
 /*
  * The following styles are auto-applied to elements with
