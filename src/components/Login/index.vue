2fa3193dcb0bffa1993890c3a52953bfa5d4ac93
Alexandre David <adavid@rheagroup.lan>
2018-05-08 14:19:33 +0100

    Login page

---
+++ src/components/Login/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <form @submit.prevent="login" class="form-signin">
+      <img src="@/assets/dfms_icon.png" class="mb-4">
+      <h2 class="h3 mb-3 font-weight-normal">Sign in</h2>
+      <label for="inputEmail" class="sr-only"></label><input class="form-control" id="inputEmail" type="email" v-model="email" placeholder="email">
+      <label for="inputPassword" class="sr-only"></label><input class="form-control" id="inputPassword" type="password" v-model="pass" placeholder="password">
+      <button type="submit" class="btn btn-lg btn-primary btn-block">login</button>
+      <p v-if="error" class="error">Bad login information</p>
+    </form>
+</template>
+
+<script>
+  import auth from '@/authentication'
+  export default {
+    data () {
+      return {
+        email: 'joe@example.com',
+        pass: '',
+        error: false
+      }
+    },
+    methods: {
+      login () {
+        auth.login(this.email, this.pass, loggedIn => {
+          if (!loggedIn) {
+            this.error = true
+          } else {
+            this.$router.replace(this.$route.query.redirect || '/')
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Map from '@/components/Map/Map'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +11,15 @@ export default new Router({
       path: '/',
       name: 'Map',
       component: Map
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '*',
+      redirect: { name: 'Map' }
     }
   ]
 })
