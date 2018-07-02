b236813fa57058c88e6ea37b42149852aa64c867
Alexandre David <adavid@rheagroup.lan>
2018-07-02 17:02:47 +0100

    Init testing

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
+  'go to auth': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
@@ -10,10 +10,21 @@ module.exports = {
 
     browser
       .url(devServer)
+      .waitForElementVisible('#auth0-lock-container-1', 5000)
+      .end()
+  },
+  'is authenticated': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#auth0-lock-container-1 input[type="email"]', 5000)
+      .setValue('#auth0-lock-container-1 input[type="email"]', 'testdfms@rheagroup.com')
+      .setValue('#auth0-lock-container-1 input[type="password"]', 'testdfms2018&')
+      .click('#auth0-lock-container-1 .auth0-lock-submit')
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
       .end()
   }
 }
