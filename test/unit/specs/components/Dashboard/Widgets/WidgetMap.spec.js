10d5caf6fb3df25d04d5c74b3c2c37b6ea4e680e
Alexandre David <adavid@rheagroup.lan>
2018-07-16 10:56:32 +0100

    WidgetMap tests

---
+++ test/unit/specs/components/Dashboard/Widgets/WidgetMap.spec.js
@@ -0,0 +1,70 @@
+import { shallowMount } from '@vue/test-utils'
+import WidgetMap from '@/components/Dashboard/Widgets/WidgetMap.vue'
+
+import MapObj from '@/store/map'
+import AreaLayer from '@/store/areaLayer'
+import DisplayedLayer from '@/store/displayedLayer'
+
+const mockMap = jest.fn()
+jest.mock('@/store/map', () => {
+  return jest.fn().mockImplementation(() => {
+    return mockMap
+  })
+})
+
+const mockAreaLayer = jest.fn()
+jest.mock('@/store/areaLayer', () => {
+  return jest.fn().mockImplementation(() => {
+    return mockAreaLayer
+  })
+})
+
+const mockDisplayedLayer = {
+  setDisplayedLayer: jest.fn()
+}
+jest.mock('@/store/displayedLayer', () => {
+  return jest.fn().mockImplementation(() => {
+    return mockDisplayedLayer
+  })
+})
+
+describe('WidgetMap.vue', () => {
+  let wrapper
+  beforeEach(async () => {
+    wrapper = shallowMount(WidgetMap, {
+      propsData: {
+        parameter: {
+          layerUrl: 'layerUrl',
+          layerParameters: 'layerParameters'
+        },
+        area: 'area',
+        widgetKey: 'widgetKey'
+      }
+    })
+  })
+
+  it('Mount with good data', () => {
+    const mapId = 'map-container-widgetwidgetKey'
+    expect(wrapper.find(`#${mapId}`)).toBeDefined()
+    expect(MapObj).toBeCalledWith(mapId)
+    expect(AreaLayer).toBeCalledWith(mockMap, 'area')
+    expect(AreaLayer).toBeCalledWith(mockMap, 'area')
+    expect(DisplayedLayer).toBeCalledWith(mockMap, 'layerUrl', 'layerParameters')
+    expect(wrapper.vm.isLoaded).toBe(true)
+  })
+
+  it('On watch parameter', () => {
+    wrapper.setProps({
+      parameter: {
+        layerUrl: 'layerUrl2',
+        layerParameters: 'layerParameters2'
+      }
+    })
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalledWith(mockMap, 'layerUrl2', 'layerParameters2')
+  })
+
+  it('Providers are correct', async () => {
+    expect(wrapper.vm.getMap()).toBe(mockMap)
+    expect(wrapper.vm.getDisplayedLayer()).toBe(mockDisplayedLayer)
+  })
+})
