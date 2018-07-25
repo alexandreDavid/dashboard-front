d225208d41a89f6eae2fda071e1023c0d17bf609
Alexandre David <adavid@rheagroup.lan>
2018-07-25 10:41:38 +0100

    Test correction

--- test/unit/specs/components/Map/ReportedLayer/ReportedLayer.spec.js
+++ test/unit/specs/components/Map/ReportedLayer/ReportedLayer.spec.js
@@ -53,7 +53,7 @@ describe('ReportedLayer.vue', () => {
     wrapper.setProps({
       selectedReportedLayer: {selectedReportedLayer: 2}
     })
-    expect(mockCircleMarker.remove).toHaveBeenCalledTimes(2)
+    expect(mockCircleMarker.remove).toHaveBeenCalledTimes(3)
     expect(wrapper.vm.allMarkers.length).toBe(wrapper.vm.mockStations().length)
   })
 
@@ -72,7 +72,7 @@ describe('ReportedLayer.vue', () => {
     expect(mockDomEvent.stop).toBeCalled()
     jest.advanceTimersByTime(0)
     expect(wrapper.vm.showSideBar).toBe(true)
-    expect(wrapper.vm.reportedDetails.station).toEqual(wrapper.vm.mockStations()[1].station)
+    expect(wrapper.vm.reportedDetails.station).toEqual(wrapper.vm.mockStations()[2].station)
   })
 
   it('Close sidebar on emit event', () => {
