b6444f1b59a6306fb16ceae3d705dea5b62be8ac
Alexandre David <adavid@rheagroup.lan>
2018-07-25 14:43:15 +0100

    Change geoserver IP

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  GEOSERVER_URL: '"http://52.56.209.230:8180/geoserver"',
   NODE_ENV: '"development"',
   TRACKER_URL: '"//127.0.0.1:8888/piwik/"',
   TRACKER_SITE_ID: '1',
