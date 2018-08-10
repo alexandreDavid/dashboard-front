415d18798ff1bb35c82be2ba5c88887b54837292
Alexandre David <adavid@rheagroup.lan>
2018-08-10 11:47:06 +0100

    Changing the API route

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   DATA_URL: '"http://demo.dfms.co.uk/data"',
-  API_URL: '"http://35.178.87.6/api"',
+  API_URL: '"http://35.178.87.6/api/v1"',
   GEOSERVER_URL: '"http://demo.dfms.co.uk/geoserver"',
   // GEOSERVER_URL: '"http://18.130.18.23:8180/geoserver"',
   AUTH: {
