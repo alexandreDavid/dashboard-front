5c6b92fdfbc1061a21ae721c8f7b457ad38c748d
Alexandre David <adavid@rheagroup.lan>
2018-09-11 15:39:13 +0100

    Sidebar static format

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,14 +1,28 @@
 <template>
   <transition name="sidebar">
-    <div class="sidebar" v-bind:class="position">
-        <div class="dismiss">
+    <div class="sidebar" v-bind:class="[{static: isStatic}, position]">
+      <div class="d-flex flex-column h-100">
+        <div class="p-2 flex-shrink-1 dismiss" v-if="!isStatic">
           <button type="button" class="close" aria-label="Close" @click="$emit('close')">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="sidebar-container p-2">
-          <slot></slot>
+        <div class="h-100 position-relative">
+          <div class="sidebar-container p-2">
+            <slot></slot>
+          </div>
         </div>
+      </div>
+        <!-- <div class="dismiss flex-shrink-1 bg-secondary flex-shrink-1 order-1 sticky-top shadow">
+          <button type="button" class="closeclose" aria-label="Close" @click="$emit('close')">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="position-relative h-100 order-2">
+          <div id="page-container"></div> -->
+        <!-- <div class="sidebar-container p-2 h-100"> -->
+          <!-- <slot></slot> -->
+        <!-- </div> -->
     </div>
   </transition>
 </template>
@@ -21,6 +35,10 @@ export default {
     position: {
       type: String,
       default: 'left'
+    },
+    isStatic: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
