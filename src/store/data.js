7b3d84f61d97c9ad627eccd2eea8c4b65afe4b33
Alexandre David <adavid@rheagroup.lan>
2018-10-03 10:05:51 +0100

    Mock parameter grouping

--- src/store/data.js
+++ src/store/data.js
@@ -2,23 +2,37 @@ import axios from 'axios'
 
 const urlRoot = process.env.DATA_URL
 
-let structureCache
+// let structureCache
 
 export default {
   async getStructure () {
-    if (!structureCache) {
-      try {
-        const response = await axios.get(
-          `${urlRoot}/structure`
-        )
-        structureCache = response.data
-        return structureCache
-      } catch (e) {
-        return false
-      }
-    } else {
-      return structureCache
+    return {
+      parameterGroupings: [
+        {
+          groupingId: 0,
+          displayName: 'Weather forecast'
+        }, {
+          groupingId: 1,
+          displayName: 'Hydrological forecast'
+        }, {
+          groupingId: 2,
+          displayName: 'Satellite derived data'
+        }
+      ]
     }
+    // if (!structureCache) {
+    //   try {
+    //     const response = await axios.get(
+    //       `${urlRoot}/structure`
+    //     )
+    //     structureCache = response.data
+    //     return structureCache
+    //   } catch (e) {
+    //     return false
+    //   }
+    // } else {
+    //   return structureCache
+    // }
   },
   async getAreaParameterData (area, parameter) {
     try {
