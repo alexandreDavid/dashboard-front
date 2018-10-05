37c853b628f82e67bfdb18063407f5ebfd95d572
Alexandre David <adavid@rheagroup.lan>
2018-10-05 16:05:48 +0100

    Unit tests

--- test/unit/specs/components/Map/DisplayedLayer/DisplayedLayerControl.spec.js
+++ test/unit/specs/components/Map/DisplayedLayer/DisplayedLayerControl.spec.js
@@ -1,7 +1,6 @@
 import DisplayedLayerControl from '@/components/Map/DisplayedLayer/DisplayedLayerControl'
 import { shallowMount } from '@vue/test-utils'
 import TimeSerie from '@/components/Map/OverMap/OverMapControl/TimeSerie/TimeSerie'
-import DisplayedLayerControlParams from '@/components/Map/DisplayedLayer/DisplayedLayerControlParams'
 
 const mockDisplayedLayer = {
   setDisplayedLayer: jest.fn(),
@@ -15,17 +14,103 @@ function getDisplayedLayer () {
 describe('DisplayedLayerControl.vue', () => {
   let wrapper
   beforeEach(() => {
+    mockDisplayedLayer.setDisplayedLayer.mockClear()
     wrapper = shallowMount(DisplayedLayerControl, {
       provide: {
         getDisplayedLayer: getDisplayedLayer
       },
       propsData: {
-        parameter: {}
+        parameter: {
+          hasTimeFrame: true,
+          layer: 'layer',
+          type: 'type',
+          times: []
+        }
       }
     })
   })
 
-  it('Mounted correctly', () => {
+  it('Mounted correctly without model', () => {
+    mockDisplayedLayer.setDisplayedLayer.mockClear()
+    shallowMount(DisplayedLayerControl, {
+      provide: {
+        getDisplayedLayer: getDisplayedLayer
+      },
+      propsData: {
+        parameter: {
+          type: 'type',
+          times: []
+        }
+      }
+    })
     expect(mockDisplayedLayer.setDisplayedLayer).not.toBeCalled()
+  })
+
+  it('Mounted correctly with model', () => {
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalled()
+  })
+
+  it('On changeSelectedModel as interval', () => {
+    const now = Date.now() / 1000
+    const mockModel = {
+      type: 'interval',
+      times: [
+        {endTime: now - 2},
+        {endTime: now - 1},
+        {endTime: now},
+        {endTime: now + 1},
+        {endTime: now + 2}
+      ]
+    }
+    wrapper.vm.changeSelectedModel(mockModel)
+    expect(wrapper.vm.activeModel).toEqual(mockModel)
+    expect(mockDisplayedLayer.formatTime).toBeCalledWith({endTime: now + 1})
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalled()
+  })
+
+  it('On changeSelectedModel as date', () => {
+    const mockModel = {
+      type: 'date',
+      times: ['time1', 'time2', 'lasttime']
+    }
+    wrapper.vm.changeSelectedModel(mockModel)
+    expect(wrapper.vm.activeModel).toEqual(mockModel)
+    expect(mockDisplayedLayer.formatTime).toBeCalledWith('lasttime')
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalled()
+  })
+
+  it('On changeSelectedModel as no valid type', () => {
+    mockDisplayedLayer.formatTime.mockClear()
+    const mockModel = {
+      type: 'novalid',
+      times: ['time1', 'time2', 'lasttime']
+    }
+    wrapper.vm.changeSelectedModel(mockModel)
+    expect(wrapper.vm.activeModel).toEqual(mockModel)
+    expect(mockDisplayedLayer.formatTime).not.toBeCalled()
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalled()
+  })
+
+  it('On time serie changes', () => {
+    wrapper.find(TimeSerie).vm.$emit('change', 'change')
+    expect(mockDisplayedLayer.setTime).toBeCalledWith('change')
+  })
+
+  it('On parameter changes', () => {
+    mockDisplayedLayer.setDisplayedLayer.mockClear()
+    wrapper.setProps({
+      parameter: {}
+    })
+    expect(mockDisplayedLayer.setDisplayedLayer).not.toBeCalled()
+
+    wrapper.setProps({
+      parameter: {
+        hasTimeFrame: true,
+        layer: 'layer',
+        type: 'type',
+        times: []
+      }
+    })
+    expect(mockDisplayedLayer.setDisplayedLayer).toBeCalled()
   })
 })
