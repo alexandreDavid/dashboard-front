61689654f760b1cb04ed92b97fe15bd4be0d4981
Alexandre David <adavid@rheagroup.lan>
2018-10-16 15:23:41 +0100

    Font awesome refactoring

--- src/main.js
+++ src/main.js
@@ -5,13 +5,7 @@ import App from './App'
 import router from './router'
 import Tracking from './tracking'
 import Authentication from './store/authentication'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash, faCheck, faPlus, faEdit, faSave, faSpinner, faGlobeAfrica, faBars, faLocationArrow, faCircle, faInfo, faPlay, faPause, faMinus, faCaretDown, faCaretLeft, faCaretRight, faTachometerAlt, faForward, faUser, faInfoCircle, faWindowMaximize, faWindowRestore, faMap, faHistory, faChartBar, faLayerGroup, faEye, faEyeSlash, faTimes, faSearch, faDrawPolygon } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faTrash, faCheck, faPlus, faEdit, faSave, faSpinner, faGlobeAfrica, faBars, faLocationArrow, faCaretDown, faCaretLeft, faCaretRight, faCircle, faInfo, faPlay, faPause, faMinus, faTachometerAlt, faForward, faUser, faInfoCircle, faWindowMaximize, faWindowRestore, faMap, faHistory, faChartBar, faLayerGroup, faEye, faEyeSlash, faTimes, faSearch, faDrawPolygon)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import '@/utils/fontAwesome'
 
 Vue.config.productionTip = false
 
--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash, faCheck, faPlus, faEdit, faSave, faSpinner, faUndo, faBars, faLocationArrow, faCircle, faInfo, faPlay, faPause, faMinus, faCaretDown, faCaretLeft, faCaretRight, faTachometerAlt, faForward, faUser, faInfoCircle, faWindowMaximize, faWindowRestore, faMap, faHistory, faChartBar, faLayerGroup, faGlobeAfrica } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faTrash, faCheck, faPlus, faEdit, faSave, faSpinner, faUndo, faBars, faLocationArrow, faCaretDown, faCaretLeft, faCaretRight, faCircle, faInfo, faPlay, faPause, faMinus, faTachometerAlt, faForward, faUser, faInfoCircle, faWindowMaximize, faWindowRestore, faMap, faHistory, faChartBar, faLayerGroup, faGlobeAfrica)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import '@/utils/fontAwesome'
 
 Vue.config.productionTip = false
 
