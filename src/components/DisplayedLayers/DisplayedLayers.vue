7279f42c5a2de9e47fa1e4b6e7722859b5693602
Alexandre David <adavid@rheagroup.lan>
2018-10-26 12:48:44 +0100

    UI new style

--- src/components/DisplayedLayers/DisplayedLayers.vue
+++ src/components/DisplayedLayers/DisplayedLayers.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="w-100">
-    <button id="forecast-selection-btn" @click="$emit('openAddingLayerSideBar')" class="btn btn-primary shadow"><font-awesome-icon icon="plus" /> Add a layers</button>
-    <button id="save-dashboard-btn" @click="addToDashboard" class="btn btn-primary shadow"><font-awesome-icon icon="tachometer-alt" /> Add to dashboard</button>
-    <div v-for="(layer, key) in val" :key="key">
-      <displayed-layer-control v-bind:parameter="layer" @remove="remove(key)" @up="up(key)" @down="down(key)"></displayed-layer-control>
+  <div class="w-100 card shadow">
+    <div class="card-body p-2">
+      <button id="forecast-selection-btn" @click="$emit('openAddingLayerSideBar')" class="btn btn-secondary w-100"><font-awesome-icon icon="plus" /> Add coverage maps</button>
+      <div v-for="(layer, key) in val" :key="key">
+        <displayed-layer-control v-bind:parameter="layer" @remove="remove(key)" @up="up(key)" @down="down(key)"></displayed-layer-control>
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +13,6 @@
 import DisplayedLayerControl from '@/components/Map/DisplayedLayer/DisplayedLayerControl'
 
 import Parameter from '@/store/parameter'
-import Dashboard from '@/store/dashboard'
 
 export default {
   name: 'DisplayedLayers',
@@ -38,17 +38,6 @@ export default {
     }
   },
   methods: {
-    addToDashboard () {
-      const savedDashboard = Dashboard.getSavedDashboard()
-      savedDashboard.addCard({
-        title: 'Current map',
-        widget: {
-          id: 'currentmap',
-          label: 'Current map'
-        }
-      })
-      savedDashboard.save()
-    },
     remove (index) {
       this.val.splice(index, 1)
     },
