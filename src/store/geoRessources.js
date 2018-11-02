8c8ec61816180d23d8c30ba709a00a409fd467dd
Alexandre David <adavid@rheagroup.lan>
2018-11-02 16:55:06 +0000

    Adding all the geo ressources

--- src/store/geoRessources.js
+++ src/store/geoRessources.js
@@ -41,6 +41,132 @@ let parameters = [
     statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
     times: sevenDays,
     unit: 'K'
+  }, {
+    name: 'Dewpoint temperature (7 days forecast)',
+    layer: 'mogreps:dew_point_temperature',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=mogreps:dew_point_temperature`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'High cloud amount (7 days forecast)',
+    layer: 'mogreps:high_type_cloud_area_fraction',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=mogreps:high_type_cloud_area_fraction`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'Low cloud amount (7 days forecast)',
+    layer: 'mogreps:low_type_cloud_area_fraction',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=mogreps:low_type_cloud_area_fraction`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'Relative humidity (2 days forecast)',
+    layer: 'twoDaysForecast:relative_humidity',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:relative_humidity`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/relative_humidity',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Relative humidity (7 days forecast)',
+    layer: 'mogreps:low_type_cloud_area_fraction',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:relative_humidity`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'Total cloud amount (7 days forecast)',
+    layer: 'mogreps:cloud_area_fraction_assuming_maximum_random_overlap',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&STRICT=false&style=cloud_area_fraction_assuming_maximum_random_overlap`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'Total precipitation rate (2 days forecast)',
+    layer: 'twoDaysForecast:stratiform_rainfall_rate_0',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:stratiform_rainfall_rate_0`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Surface downwelling longwave flux (2 days forecast)',
+    layer: 'twoDaysForecast:surface_downwelling_longwave_flux',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:surface_downwelling_longwave_flux`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Surface atmospheric temperature (2 days forecast)',
+    layer: 'twoDaysForecast:surface_temperature',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:surface_temperature`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'TOA outgoing longwave flux (2 days forecast)',
+    layer: 'twoDaysForecast:toa_outgoing_longwave_flux',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=twoDaysForecast:toa_outgoing_longwave_flux`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Visibility (7 days forecast)',
+    layer: 'mogreps:visibility_in_air',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=mogreps:visibility_in_air`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: sevenDays,
+    unit: 'K'
+  }, {
+    name: 'Baseflow',
+    layer: 'hidrology:OUT_BASEFLOW',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=hidrology:OUT_BASEFLOW`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Evapotranspiration',
+    layer: 'hidrology:OUT_EVAP',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=hidrology:OUT_EVAP`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Surface runoff',
+    layer: 'hidrology:OUT_RUNOFF',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=hidrology:OUT_RUNOFF`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'NDVI',
+    layer: 'env_sys:ndvi',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=env_sys:ndvi`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'BAI',
+    layer: 'env_sys:bai',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=env_sys:bai`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Land surface temperature',
+    layer: 'pixalytics:land_surface_temperature',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=pixalytics:land_surface_temperature`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
+  }, {
+    name: 'Soil moisture content',
+    layer: 'pixalytics:soil_moisture_content',
+    legend: `${process.env.GEOSERVER_URL}/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=pixalytics:soil_moisture_content`,
+    statistics: 'http://demo.dfms.co.uk/data/:type/:name/air_temperature',
+    times: twoDays,
+    unit: 'K'
   }
 ]
 const defaultFormat = {
