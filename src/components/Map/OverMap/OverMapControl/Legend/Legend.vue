f85c3190ce5df6028ff7933d7db1383d1095bf3c
Alexandre David <adavid@rheagroup.lan>
2018-11-16 12:12:45 +0000

    The legend get the ranges

--- src/components/Map/OverMap/OverMapControl/Legend/Legend.vue
+++ src/components/Map/OverMap/OverMapControl/Legend/Legend.vue
@@ -1,63 +1,42 @@
 <template>
   <div class="legend-displaying" v-if="legend && legend.values && legend.values.length">
-    <div class="position-relative d-flex align-items-center">
+    <div class="position-relative d-flex align-items-center" :class="{'mb-3': legend.range}">
       <div class="mr-1 label">{{ legend.values[0].name }}</div>
-      <div class="gradient flex-grow-1" v-bind:style="{ background: getGradientColor(legend.values) }"></div>
+      <div class="gradient flex-grow-1 position-relative" v-bind:style="{ background: getGradientColor(legend.values) }">
+        <div v-if="legend.range" class="position-absolute range" v-bind:style="calculateRangePlacement(legend)">
+          <div class="position-absolute pr-1 range-label min">{{ legend.range.min.name }}</div>
+          <div class="position-absolute pl-1 range-label max">{{ legend.range.max.name }}</div>
+        </div>
+      </div>
       <div class="ml-1 label">{{ legend.values[legend.values.length - 1].name }}</div>
     </div>
   </div>
 </template>
 
 <script>
-// import Unit from '@/utils/unit'
-import Settings from '@/store/settings'
-
 export default {
   name: 'Legend',
   props: [
     'legend'
   ],
-  data () {
-    return {
-      // unitFamily: false,
-      // activeUnit: false,
-      activeUnits: Settings.activeSettings,
-      isLoaded: false
-    }
-  },
   methods: {
     getGradientColor (values) {
       return `linear-gradient(to right, ${values.map(d => this.convertHex(d.color)).join(', ')})`
     },
-    // changeActiveUnit (unit) {
-    //   this.$set(this, 'activeUnit', unit)
-    //   this.activeUnitLabel = Unit.getLabel(unit)
-    // },
     convertHex (hex) {
       const r = parseInt(hex.substring(1, 3), 16)
       const g = parseInt(hex.substring(3, 5), 16)
       const b = parseInt(hex.substring(5, 7), 16)
       return `rgba(${r}, ${g}, ${b})`
+    },
+    calculateRangePlacement (legend) {
+      const firstValue = legend.values[0].value
+      const lastValue = legend.values[legend.values.length - 1].value
+      return {
+        left: `${(legend.range.min.value - firstValue) / (lastValue - firstValue) * 100}%`,
+        width: `${(legend.range.max.value - legend.range.min.value) / (lastValue - firstValue) * 100}%`
+      }
     }
-  // },
-  // filters: {
-  //   convert: function (value, from, to) {
-  //     if (from !== to) {
-  //       value = Unit.convert(from, to, value)
-  //     }
-  //     return value
-  //   }
-  // },
-  // watch: {
-  //   activeUnits: {
-  //     async handler (val) {
-  //       // this.isLoaded = false
-  //       // this.legend = await this.layer.getLegend()
-  //       // this.isLoaded = true
-  //       // this.changeActiveUnit(val[this.unitFamily])
-  //     },
-  //     deep: true
-    // }
   }
 }
 </script>
@@ -73,6 +52,26 @@ export default {
 
   .label {
     font-size: 0.8em;
+  }
+
+  .range {
+    border-left: 2px solid #888;
+    border-right: 2px solid #888;
+    height: 30px;
+
+    .range-label {
+      font-size: 0.7em;
+      width: max-content;
+      bottom: 0;
+      border-bottom: 2px solid #888;
+
+      &.min {
+        right: 100%;
+      }
+      &.max {
+        left: 100%;
+      }
+    }
   }
 }
 </style>
