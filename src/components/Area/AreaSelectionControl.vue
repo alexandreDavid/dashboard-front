b1ab73492b978ada6ac8071eadd46a83c43a0b9c
Alexandre David <adavid@rheagroup.lan>
2018-11-20 12:57:08 +0000

    New Area selection mode

--- src/components/Area/AreaSelectionControl.vue
+++ src/components/Area/AreaSelectionControl.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card">
-    <div class="card-body p-2">
+  <div class="card" v-bind:class="{'bg-primary text-light': !edit}">
+    <div class="card-body p-2" v-if="edit">
       <h6>Place selection</h6>
       <SearchLocation v-if="!drawMode" class="w-100 mb-2" v-model="val" @input="onSearchLocationSelected" @locate="zoom" />
       <area-selection-draw-controls v-if="drawMode" :drawMode="drawMode" @cancel="cancelDrawing" @validate="saveCustomLocation" @changeMode="switchDrawMode">
@@ -12,6 +12,17 @@
         <button type="button" class="btn btn-secondary" @click="startDrawMode('rectangle')" v-bind:class="{active: drawMode === 'rectangle'}"><font-awesome-icon icon="vector-square" /></button>
       </div>
       <button type="button" class="btn btn-secondary btn-sm" disabled><font-awesome-icon icon="file-import" /> Import from file</button>
+    </div>
+    <div class="card-footer" v-if="edit">
+      <button type="button" class="btn btn-secondary btn-sm" @click="toggleEdit">Cancel</button>
+    </div>
+    <div class="card-body p-2" v-else>
+      <h6>Selected place</h6>
+      <div>
+        <label class="mr-1">{{ val.name }}</label>
+        <button type="button" class="btn btn-light btn-sm" @click="zoom"><font-awesome-icon icon="search" /> Locate</button>
+        <button type="button" class="btn btn-light btn-sm" @click="toggleEdit"><font-awesome-icon icon="edit" /> Edit</button>
+      </div>
     </div>
   </div>
 </template>
@@ -28,31 +39,44 @@ export default {
   data () {
     return {
       drawMode: false,
-      val: false
+      val: false,
+      edit: false,
+      savedLayer: false
     }
   },
   mounted () {
     this.val = Area.getSelectedArea()
+    this.edit = !this.val
   },
   methods: {
     zoom () {
       this.getAreaLayer().zoomToArea()
     },
+    toggleEdit () {
+      this.edit = !this.edit
+    },
     async onSearchLocationSelected (val) {
       this.val = val
-      Area.setSelectedArea(val)
-      await this.getAreaLayer().setSelectedArea(val)
-      this.$emit('change', val)
+      if (val) {
+        Area.setSelectedArea(val)
+        await this.getAreaLayer().setSelectedArea(val)
+        this.$emit('change', val)
+        this.edit = false
+      }
     },
     switchDrawMode (mode) {
       this.drawMode = mode
     },
     startDrawMode (mode) {
+      this.savedLayer = this.getAreaLayer().getSelectedArea()
       this.getAreaLayer().remove()
       this.drawMode = mode
     },
     cancelDrawing () {
-      this.getAreaLayer().add()
+      if (this.savedLayer) {
+        this.getAreaLayer().setSelectedArea(this.savedLayer)
+        this.getAreaLayer().add()
+      }
       this.drawMode = false
     },
     saveCustomLocation (newArea) {
