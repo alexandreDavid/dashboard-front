82afab0e63a1e380a91a763b14ba22ad9fc71be1
Alexandre David <adavid@rheagroup.lan>
2018-11-22 14:15:16 +0000

    Changing catalogue URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   DATA_URL: '"http://demo.dfms.co.uk/data"',
   API_URL: '"http://35.178.87.6/api/v1"',
-  CATALOGUE_API_URL: '"http://a60a733e9e2a411e8bd850258108f7fd-814591218.eu-west-1.elb.amazonaws.com"',
+  CATALOGUE_API_URL: '"http://a4255e5baee6011e8bd850258108f7fd-1365817557.eu-west-1.elb.amazonaws.com"',
   GEOSERVER_URL: '"http://35.178.230.72:8180/geoserver"',
   AUTH: {
     domain: '"testada.eu.auth0.com"',
