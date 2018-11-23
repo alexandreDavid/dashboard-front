325f90f11499498da05c73a58890dfab25e6a67c
Alexandre David <adavid@rheagroup.lan>
2018-11-23 12:28:47 +0000

    Changing env variables in the dev version

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,9 +11,17 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : require('../config/prod.env')
+let env
+switch (process.env.NODE_ENV) {
+  case 'testing':
+    env = require('../config/test.env')
+    break;
+  case 'dev':
+    env = require('../config/dev.env')
+    break;
+  default:
+    env = require('../config/prod.env')
+}
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
