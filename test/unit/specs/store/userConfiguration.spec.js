64cf5d447b87deca2c39c7c85b296749d167dfa0
Alexandre David <adavid@rheagroup.lan>
2019-01-17 10:32:27 +0000

    Unit test correction

--- test/unit/specs/store/userConfiguration.spec.js
+++ test/unit/specs/store/userConfiguration.spec.js
@@ -2,7 +2,7 @@ import UserConfiguration from '@/store/userConfiguration'
 
 describe('userConfiguration.js', () => {
   it('Init correctly', async () => {
-    expect(Object.keys(UserConfiguration).length).toEqual(14)
+    expect(Object.keys(UserConfiguration).length).toEqual(21)
   })
 
   it('Calls get successful without value', () => {
