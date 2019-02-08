6aa04e10e3a062ac80bf899ff5943186370449ec
Alexandre David <a.david@rheagroup.com>
2019-02-08 09:43:50 +0000

    EditableTextArea height managing

--- src/components/EditableField/EditableTextArea.vue
+++ src/components/EditableField/EditableTextArea.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <button class="btn btn-link" @click="edit" v-if="addButtonLabel && !isEditing && !value">{{ addButtonLabel }}</button>
+    <button class="btn btn-link" @click="editTextArea" v-if="addButtonLabel && !isEditing && !value">{{ addButtonLabel }}</button>
     <div v-else-if="isEditing">
-      <textarea v-model="val" class="form-control mb-1" :placeholder="placeholder" :aria-label="placeholder"></textarea>
+      <textarea :rows="textAreaRows" v-model="val" class="form-control mb-1" :placeholder="placeholder" :aria-label="placeholder"></textarea>
       <div class="float-right">
         <button class="btn btn-secondary" type="button" @click="isEditing = false">Cancel</button>
         <button class="btn btn-success" type="button" @click="save(val)">Save</button>
       </div>
     </div>
-    <span class="px-3 py-1" @click="edit" v-else>{{ value }}</span>
+    <pre class="px-3 py-1" @click="editTextArea" v-else>{{ value }}</pre>
   </div>
 </template>
 
@@ -18,6 +18,18 @@ import EditableMixins from './EditableMixins'
 export default {
   name: 'EditableTextArea',
   props: ['addButtonLabel'],
-  mixins: [EditableMixins]
+  data () {
+    return {
+      textAreaRows: 2
+    }
+  },
+  mixins: [EditableMixins],
+  methods: {
+    editTextArea () {
+      // TextArea height is the number of lines (line breaks + 1) or 2
+      this.textAreaRows = ((this.value.match(/(\r\n|\n)/g) || ['']).length) + 1
+      this.edit()
+    }
+  }
 }
 </script>
