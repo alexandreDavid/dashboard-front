1e20515c713514d98650164f1844e7d2a2b5d492
Alexandre David <a.david@rheagroup.com>
2019-02-18 15:40:51 +0000

    Fix description issue

--- src/store/dashboard.js
+++ src/store/dashboard.js
@@ -14,7 +14,7 @@ export default class Dashboard {
     widget.id = Math.max(...this.widgets.map(w => {
       return w.id
     }), 0) + 1
-    widget.description = ''
+    widget.description = widget.description || ''
     widget.colIndex = 0
     this.widgets.push(widget)
     return widget
