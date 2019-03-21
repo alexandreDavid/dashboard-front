b53e1341407cfcc52da2905573d561e9829f0462
Alexandre David <a.david@rheagroup.com>
2019-03-21 11:24:18 +0000

    Fix inte

--- src/updates/list/1.js
+++ src/updates/list/1.js
@@ -5,7 +5,7 @@ import UserConfiguration from '@/store/userConfiguration'
  */
 export default function () {
   const baseMap = UserConfiguration.getActiveBaseMapLayer()
-  if (!baseMap || !baseMap.includes('https') || baseMap === 'https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png') {
+  if (!baseMap || typeof baseMap !== 'string' || !baseMap.includes('https') || baseMap === 'https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png') {
     UserConfiguration.setActiveBaseMapLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png')
   }
 }
