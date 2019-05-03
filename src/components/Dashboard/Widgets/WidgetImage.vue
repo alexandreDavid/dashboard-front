3bd0919b5c4cc8d8bfe7cc195ac908d161d6979f
Alexandre David <a.david@rheagroup.com>
2019-05-03 10:22:10 +0100

    Fix image resizing

--- src/components/Dashboard/Widgets/WidgetImage.vue
+++ src/components/Dashboard/Widgets/WidgetImage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card-body p-0">
-    <img class="w-100" :src="config.src">
+  <div class="card-body p-0 m-auto">
+    <img style="max-width: 100%; height: auto;" :src="config.src">
     <div v-if="config.description" class="widget-description px-3 py-1 mb-0">{{ config.description }}</div>
   </div>
 </template>
