bc54c7cc4ccb563dd90b5dd0237dab01223a9df2
codingMonkey <codingmonkeydev@hotmail.com>
2019-06-10 13:35:00 +0100

    Fixing #30 wired up route for SettingsPage component in the Profile sidebar (Profile\index.vue) and removed the "close" <button/> (no longer needed).  refactoring of SettingsPage still to follow

--- src/components/Settings/SettingsPage.vue
+++ src/components/Settings/SettingsPage.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="settingsPage" class="m-3">
     <h2 class="text-left mb-4">
-      <button type="button" class="close" aria-label="Close" @click="close()">
-        <span aria-hidden="true">&times;</span>
-      </button>
       Settings menu
     </h2>
     <Settings class="m-3"/>
