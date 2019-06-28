4a302d3fbab40fcbd63b6d8efd68b2d84e237f47
Alexandre David <a.david@rheagroup.com>
2019-06-28 10:55:12 +0100

    Unit tests

--- test/unit/specs/router/index.spec.js
+++ test/unit/specs/router/index.spec.js
@@ -1,11 +1,10 @@
 import Router from '@/router'
 import Auth from '@/store/authentication'
 
-const mockNext = jest.fn()
-
 jest.mock('@/store/authentication', () => ({
   isAuthenticated: jest.fn(),
-  handleAuthentication: jest.fn()
+  handleAuthentication: jest.fn(),
+  logout: jest.fn()
 }))
 
 jest.mock('@/components/Dashboard/DashboardPage', () => ({
@@ -14,21 +13,56 @@ jest.mock('@/components/Dashboard/DashboardPage', () => ({
 }))
 
 describe('router.js', () => {
+  beforeEach(() => {
+    Auth.isAuthenticated.mockClear()
+    Auth.logout.mockClear()
+  })
+
   it('Calls init', () => {
     expect(Router.options.routes.length).toBe(4)
   })
 
-  // it('checkAuth without authentication', () => {
-  //   Router.options.routes[0].beforeEnter()
-  //   expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
-  //   expect(Auth.handleAuthentication).toHaveBeenCalledTimes(1)
-  // })
+  it('Token with authentication', () => {
+    const mockNext = jest.fn()
+    Auth.isAuthenticated.mockReturnValue(true)
+    Router.options.routes[0].beforeEnter(false, false, mockNext)
+    expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
+    expect(mockNext).toHaveBeenCalledWith('/')
+  })
 
-  // it('checkAuth with authentication', () => {
-  //   Auth.isAuthenticated.mockReturnValue(true)
-  //   Router.options.routes[0].beforeEnter(false, false, mockNext)
-  //   expect(Auth.isAuthenticated).toHaveBeenCalledTimes(2)
-  //   expect(Auth.handleAuthentication).toHaveBeenCalledTimes(1)
-  //   expect(mockNext).toHaveBeenCalledTimes(1)
-  // })
+  it('Token without authentication', () => {
+    const mockNext = jest.fn()
+    Auth.isAuthenticated.mockReturnValue(false)
+    Router.options.routes[0].beforeEnter(false, false, mockNext)
+    expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
+    expect(mockNext).toHaveBeenCalledWith()
+  })
+
+  it('CheckAuth with authentication', async () => {
+    const mockNext = jest.fn()
+    Auth.isAuthenticated.mockReturnValue(true)
+    await Router.options.routes[1].beforeEnter(false, false, mockNext)
+    expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
+    expect(mockNext).toHaveBeenCalled()
+  })
+
+  it('CheckAuth without authentication', async () => {
+    const mockNext = jest.fn()
+    Auth.isAuthenticated.mockReturnValue(false)
+    await Router.options.routes.find(r => r.path === '/').beforeEnter(false, false, mockNext)
+    expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
+    expect(Auth.logout).toHaveBeenCalledTimes(1)
+    expect(mockNext).not.toHaveBeenCalled()
+  })
+
+  it('CheckAuth error', async () => {
+    const mockNext = jest.fn()
+    Auth.isAuthenticated.mockImplementation(() => {
+      throw new Error()
+    })
+    await Router.options.routes.find(r => r.path === '/').beforeEnter(false, false, mockNext)
+    expect(Auth.isAuthenticated).toHaveBeenCalledTimes(1)
+    expect(Auth.logout).toHaveBeenCalled()
+    expect(mockNext).not.toHaveBeenCalled()
+  })
 })
